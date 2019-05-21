d046a94ce7139ae73338f8ea98d31e0f7eb482c3
LBallack <50371821+LBallack@users.noreply.github.com>
2019-05-21 18:35:56 +0800

    Initial commit

---
+++ src/main/java/com/huawei/cn/springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.huawei.cn.springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootDemoApplication {
+    @RequestMapping("/")
+    public String say(){
+        return "Hello Java,I LOVE YOU";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
